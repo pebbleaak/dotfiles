5cf67ad8383273d0990f8a622cdde2daba0182d5
Awais Ali <cuteaistories@hotmail.com>
2025-09-20 22:31:54 +0500

    Added clang formatter and a lazy plugin>

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,29 @@
+return {
+  -- lazy.nvim can manage itself (optional)
+  { "folke/lazy.nvim", lazy = false, priority = 1000 },
+
+  -- C/C++ autoformatter using clang-format
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" }, -- lazy-load
+    opts = {
+      formatters_by_ft = {
+        c   = { "clang_format" },
+        cpp = { "clang_format" },
+        h =   { "clang_format" },
+        hpp = { "clang_format" },
+      },
+      format_on_save = { lsp_fallback = true, timeout_ms = 500 },
+    },
+    keys = {
+      {
+        "<leader>f",
+        function() require("conform").format({ async = true }) end,
+        desc = "Format buffer",
+      },
+    },
+  },
+
+  -- add more plugins below this line
+}
+
