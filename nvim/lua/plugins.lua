ca713933a922166762e64a60fcdd2bf83583c866
Awais Ali <cuteaistories@hotmail.com>
2025-10-08 21:29:44 +0500

    added status line

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -178,6 +178,23 @@ return {
   end
 },
 
+{
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = { theme = 'auto' },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
+  end
+}
 --{
 --  'akinsho/toggleterm.nvim',
  -- version = "*",
